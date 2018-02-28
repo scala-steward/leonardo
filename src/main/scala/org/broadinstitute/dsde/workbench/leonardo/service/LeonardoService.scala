5fe892ee90861e7140002a1389ff2755968d438e
Rob Title <rtitle@broadinstitute.org>
2018-02-28 12:12:27 -0500

    Fix bugs, it's working now

--- src/main/scala/org/broadinstitute/dsde/workbench/leonardo/service/LeonardoService.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/leonardo/service/LeonardoService.scala
@@ -244,7 +244,7 @@ class LeonardoService(protected val dataprocConfig: DataprocConfig,
 
         _ <- dbRef.inTransaction { _.clusterQuery.updateClusterStatus(cluster.googleId, ClusterStatus.Stopping) }
       } yield {
-        clusterMonitorSupervisor ! ClusterStopped(cluster)
+        clusterMonitorSupervisor ! ClusterStopped(cluster.copy(status = ClusterStatus.Stopping))
       }
 
     } else Future.successful(())
@@ -269,7 +269,7 @@ class LeonardoService(protected val dataprocConfig: DataprocConfig,
 
         _ <- dbRef.inTransaction { _.clusterQuery.updateClusterStatus(cluster.googleId, ClusterStatus.Starting) }
       } yield {
-        clusterMonitorSupervisor ! ClusterStarted(cluster)
+        clusterMonitorSupervisor ! ClusterStarted(cluster.copy(status = ClusterStatus.Starting))
       }
 
     } else Future.successful(())
